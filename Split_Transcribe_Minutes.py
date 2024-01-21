f35f9e61689b46a200bb332725c50b6c2a5b3ed8
GEScott71 <GEScott71@gmail.com>
2024-01-21 09:48:47 -0600

    Initial commit with modified approach of prompts outside of API call function, and example with UNP earnings call minutes

--- Split_Transcribe_Minutes.py
+++ Split_Transcribe_Minutes.py
@@ -12,6 +12,13 @@
 # Last, it combines them into a single text file
 # Input: mp3 audio file
 # Output: 2 text files:  transcription.txt and minutes.txt
+#
+# v3 updates:
+# - Removed timings, and added token tracking
+# - Changed to use single function for GPT API call, and moved various prompts outside of the API call function
+# - Changed to provide the transcript as a system message and the individual prompts as user mesages, and added each to message history
+# - No clear change in performance vs. v1
+
 from pydub import AudioSegment
 import math
 import os
@@ -20,7 +27,6 @@ from openai import OpenAI
 import time
 import json
 from datetime import date
-import csv
 
 openai.api_key = open(r"C:\Users\GESco\Documents\key.txt", "r").read().strip('\n')
 client = OpenAI(
@@ -78,213 +84,104 @@ def transcribe_audio_list(segments):
     return combined_transcription
 
 
-def abstract_summary_extraction(transcription):
+def gpt_minutes(message_history):
     response = client.chat.completions.create(
         model = gpt_model,
         temperature=0,
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a highly skilled AI trained in language comprehension and summarization. I would like you to read the following transcription of a meeting and summarize it into a concise abstract paragraph. Aim to retain the most important points, providing a coherent and readable summary that could help a person understand the main points of the discussion without needing to read the entire text. Please avoid unnecessary details or tangential points."
-            },
-            {
-                "role": "user",
-                "content": transcription
-            }
-        ]
+        messages=message_history
     )
-    # return response['choices'][0]['message']['content']  # Format from old API
-    # response = response.choices[0].message.content
-    return response
-
-
-def key_points_extraction(transcription):
-    response = client.chat.completions.create(
-        model=gpt_model,
-        temperature=0,
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a proficient AI with a specialty in distilling information into key points. Based on the following text, identify and list the main points that were discussed or brought up. These should be the most important ideas, findings, or topics that are crucial to the essence of the discussion. Your goal is to provide a list that someone could read to quickly understand what was talked about."
-            },
-            {
-                "role": "user",
-                "content": transcription
-            }
-        ]
-    )
-    response = response.choices[0].message.content
-    return response
-
-
-def action_item_extraction(transcription):
-    response = client.chat.completions.create(
-        model=gpt_model,
-        temperature=0,
-        messages=[
-            {
-                "role": "system",
-                "content": "You are an AI expert in analyzing conversations and extracting action items. Please review the text and identify any tasks, assignments, or actions that were agreed upon or mentioned as needing to be done. These could be tasks assigned to specific individuals, or general actions that the group has decided to take. Please list these action items clearly and concisely."
-            },
-            {
-                "role": "user",
-                "content": transcription
-            }
-        ]
-    )
-    response = response.choices[0].message.content
-    return response
-
-
-def participant_list(transcription):
-    response = client.chat.completions.create(
-        model=gpt_model,
-        temperature=0,
-        messages=[
-            {
-                "role": "system",
-                "content": "You are an AI expert in analyzing conversations and extracting names and roles of the people speaking. Please review the text and identify each person named in the discussions, their title or role, and any other personal information they provide such as location.  Be sure to review the entire conversation and include new people named later in the meeting.  The meeting may be a company earnings conference call with analysts; if this is the case be sure to include the analysts asking questions later in the call.  Please list all of the the names and their related information clearly and concisely.  If there are clear groups of people, such as customer and supplier, group them accordingly"
-            },
-            {
-                "role": "user",
-                "content": transcription
-            }
-        ]
-    )
-    response = response.choices[0].message.content
-    return response
-
-
-def ioi_extraction(transcription):  # Items of interest
-    with open('ioi.txt', 'r') as file:  # Read items of interest from file
-        ioi = file.read()
-    ioi = ioi.replace('\n', ', ').strip(', ')  # Replace line breaks with commas
-    content = "You are a helpful and very talented AI trained to search and analyze meeting transcripts.  Carefully search the transcript provided for the following terms.  For each of the terms found, do 2 things: 1) repeat the quote where the term was used, and 2)explain your expert interpretation of what was meant by the discussion about that term.  Provide the output in an organized way.  When complete, review the transcription again to ensure none of the specified terms were missed.  Here is the list of terms: "
-    content += ioi
-    response = client.chat.completions.create(
-        model = gpt_model,
-        temperature=0,
-        messages=[
-            {
-                "role": "system",
-                "content": content
-            },
-            {
-                "role": "user",
-                "content": transcription
-            }
-        ]
-    )
-    # response = response.choices[0].message.content
-    return response
-
-
-def sentiment_analysis(transcription):
-    response = client.chat.completions.create(
-        model=gpt_model,
-        temperature=0,
-        messages=[
-            {
-                "role": "system",
-                "content": "As an AI with expertise in language and emotion analysis, your task is to analyze the sentiment of the following text. Please consider the overall tone of the discussion, the emotion conveyed by the language used, and the context in which words and phrases are used. Indicate whether the sentiment is generally positive, negative, or neutral, and provide brief explanations for your analysis where possible."
-            },
-            {
-                "role": "user",
-                "content": transcription
-            }
-        ]
-    )
-    response = response.choices[0].message.content
-    return response
-
+    print(response)
+    reply_content = response.choices[0].message.content
+    message_history.append({"role": "assistant", "content": reply_content})
+    tokens.append(response.usage.total_tokens)
+    return reply_content
 
 if __name__ == '__main__':
 
     today = str(date.today())
-    file_prefix = "Ford_Q3_2023_earnings_call"  # filename without extension to use for loading and writing various files
+    file_prefix = "unp_Q3_2023_earnings_call"  # filename without extension to use for loading and writing various files
 
     gpt_model = "gpt-4-1106-preview"
-    # gpt_model = "gpt-4-32k"
-    # gpt_model = "gpt-3.5-turbo-16k-0613"
+
 
     # Segment audio file into smaller chunks if needed
-    t0 = time.time()
-    segments = split_mp3('Data/' + file_prefix +'.mp3')  # Split mp3 into segments small enough to transcribe
-    t1 = time.time()
-
+    # segments = split_mp3('Data/' + file_prefix +'.mp3')  # Split mp3 into segments small enough to transcribe
+    #
     # Transcribe audio
-    transcription = transcribe_audio_list(segments)  # Transcribe each segment and return single combined transcription
-    t2 = time.time()
+    # transcription = transcribe_audio_list(segments)  # Transcribe each segment and return single combined transcription
+    #
+    # Save transcription as file
+    # with open(r'Data/' + file_prefix + '_transcript_' + today + '.txt','w') as file:
+    #     file.write(transcription)
 
-    with open(r'Data/' + file_prefix + '_transcript_' + today + '.txt',
-              'w') as file:  # Save transcription as file
-        file.write(transcription)
-
-    # with open(r'Data/unp_Q3_2023_earnings_call_transcript_2023-12-23.txt',
-    #           'r') as file:  # Read transcription from file
-    #     transcription = file.read()
+    # Read transcription from file
+    with open(r'Data/unp_Q3_2023_earnings_call_transcript_2023-12-22.txt','r') as file:
+        transcription = file.read()
 
     print('\n *** Transcription ***\n')
     print(transcription)
-    t3 = time.time()
 
-    # Create sections of meeting minutes
-    summary_response = abstract_summary_extraction(transcription)
-    summary = "\n Summary: \n" + summary_response.choices[0].message.content
-    # summary = "\n Summary: \n" + abstract_summary_extraction(transcription)
-    t4 = time.time()
+    message_history=[]
+    tokens=[]
 
-    key_points = "\n\n Key Points \n" + key_points_extraction(transcription)
-    t5 = time.time()
+    # Initialize with transcription
+    content = "You are a helpful and highly skilled AI trained in language comprehension and summarization. Follows is a transcription of a business meeting.  I will be asking you questions about this meeting. For each request, go back and evaluate the full transcript carefully before responding. Do not reply to this initial message - wait for further instructions.  Here is the transcription: "
+    content += transcription
+    message_history.append({"role": "system", "content": content})
+    response = gpt_minutes(message_history)
+    print(response)
 
-    action_items = "\n\n Action Items \n" + action_item_extraction(transcription)
-    t6 = time.time()
+    header = "\n --> Summary ***\n"
+    message_history.append({"role": "user", "content": "1 - Summary:  Please read the transcription provided above of a meeting and summarize it into a concise abstract paragraphs. Aim to retain the most important points, providing a coherent and readable summary that could help a person understand the main points of the discussion without needing to read the entire text. Please avoid unnecessary details or tangential points. The meeting may be a company earnings call with analysts. In this case, provide 2 summary paragraphs - a paragraph summarizing the company officers report in the first part of the call, and a 2nd paragraph summarizing the question and answer session with analysts in the latter part of the call.  Title this response --->SUMMARY<---"})
+    response = gpt_minutes(message_history)
+    print(response)
+    minutes = header
+    minutes += response
 
-    participants = "\n\n Participants \n" + participant_list(transcription)
-    t7 = time.time()
+    header = "\n\n --> Items Of Interest ***\n"
+    with open('ioi.txt', 'r') as file:  # Read items of interest from file
+        ioi = file.read()
+    ioi = ioi.replace('\n', ', ').strip(', ')  # Replace line breaks with commas
+    content = "2 - Items of Interest:  You are a helpful and very talented AI trained to search and analyze meeting transcripts.  Carefully search the transcript provided for the following terms.  For each of the terms found, do 2 things: 1) repeat the quote where the term was used, and 2)explain your expert interpretation of what was meant by the discussion about that term.  If a term is not mentioned, only state that it is not mentioned - do not provide any commentary or interpretation of that term.  When complete, review the transcription again to ensure none of the specified terms were missed.  Provide the output in an organized way. Title this response --->ITEMS OF INTEREST<--- Here is the list of terms: "
+    content += ioi
+    message_history.append({"role": "user", "content": content})
+    response = gpt_minutes(message_history)
+    print(response)
+    minutes += header
+    minutes += response
 
-    ioi_response = ioi_extraction(transcription)
-    ioi_discussion = "\n\n Items of Interest \n" + ioi_response.choices[0].message.content
-    # ioi_discussion = "\n\n Items of Interest \n" + ioi_extraction(transcription)
-    t8 = time.time()
+    header = "\n\n --> Key Points ***\n"
+    message_history.append({"role": "user", "content": "3 - Key Points:  You are a proficient AI with a specialty in distilling information into key points. Based on the transcription provided, identify and list the main points that were discussed or brought up. These should be the most important ideas, findings, or topics that are crucial to the essence of the discussion. Your goal is to provide a list that someone could read to quickly understand what was talked about. Title this response --->KEY POINTS<---"})
+    response = gpt_minutes(message_history)
+    print(response)
+    minutes += header
+    minutes += response
 
-    sentiment = "\n\n Sentiment Analysis \n" + sentiment_analysis(transcription)
-    t9 = time.time()
+    header = "\n\n --> Action Items ***\n"
+    message_history.append({"role": "user", "content": "4 - Action Items:  You are an AI expert in analyzing conversations and extracting action items.  Please review the transcription provided and identify any tasks, assignments, or actions that were agreed upon or mentioned as needing to be done. These could be tasks assigned to specific individuals, or general actions that the group has decided to take. Please list these action items clearly and concisely. Title this response --->ACTION ITEMS<---"})
+    response = gpt_minutes(message_history)
+    print(response)
+    minutes += header
+    minutes += response
 
-    # Create combined minutes
-    minutes = summary
-    minutes += key_points
-    minutes += action_items
-    minutes += participants
-    minutes += ioi_discussion
-    minutes += sentiment
+    header = "\n\n --> Sentiment Analysis ***\n"
+    message_history.append({"role": "user", "content": "5 - Sentiment Analysis:  As an AI with expertise in language and emotion analysis, your task is to analyze the sentiment of the transcription provided. Please consider the overall tone of the discussion, the emotion conveyed by the language used, and the context in which words and phrases are used. Indicate whether the sentiment is generally positive, negative, or neutral, and provide brief explanations for your analysis where possible. Title this response --->SENTIMENT ANALYSIS<---"})
+    response = gpt_minutes(message_history)
+    print(response)
+    minutes += header
+    minutes += response
 
-    print('\n *** Minutes: ***\n')
-    print(minutes)
+    header = "\n\n --> Participants ***\n"
+    message_history.append({"role": "user", "content": "6 - Participants:  Analyze the transcription provided and extract the names and roles of the people speaking. Do this by reviewing the text and identifying each person named in the discussions, their title or role, and any other personal information they provide such as location.  Be sure to review the entire conversation and include new people named later in the meeting.  The meeting may be a company earnings conference call with analysts; if this is the case be sure to include the analysts asking questions later in the call.  Please list all of the the names and their related information clearly and concisely.  If there are clear groups of people, such as customer and supplier, group them accordingly.  Provide the output in a table format. Title this response --->PARTICIPANTS<---"})
+    response = gpt_minutes(message_history)
+    print(response)
+    minutes += header
+    minutes += response
+
 
     with open(r'Data/' + file_prefix + '_minutes_' + today + '.txt','w') as file:  # Save minutes
         file.write(minutes)
-    t10 = time.time()
 
-    # Store time durations with labels in a dictionary
-    time_durations = {
-        'Segment Time': t1 - t0,
-        'Transcribe Time': t2 - t1,
-        'Print Time': t3 - t2,
-        'Summary Time': t4 - t3,
-        'Key Points Time': t5 - t4,
-        'Action Items Time': t6 - t5,
-        'Participants Time': t7 - t6,
-        'IOI Time': t8 - t7,
-        'Sentiment Time': t9 - t8,
-        'File Time': t10 - t9,
-        'Total Time': t10 - t0
-    }
-
-    with open(r'Data/' + file_prefix + '_time_durations_' + today + '.txt','w') as file:  # Write durations to a text file
-        for label, duration in time_durations.items():
-            file.write(f'{label}: {duration}\n')
-
-    print(json.dumps((summary_response.model_dump()), indent=2)) # print full output of summary_response
-    print(json.dumps((ioi_response.model_dump()), indent=2)) # print full output of ioi_response
-
+    print('\n\n ---> Minutes: ***\n')
+    print(minutes)
+    print("Tokens: ", tokens)
